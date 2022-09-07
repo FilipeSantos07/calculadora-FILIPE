ea2c41bd7dc892e2c3bff8c0c886a7935ba396a3
Filipe Santos <outerspaceguy@live.co.uk>
2022-09-07 12:43:25 -0300

    calculadora

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-function limpar() {
-    var resultado = document.querySelector("#resultado")
-    resultado.innerHTML = ""
+var resultado = document.querySelector("#resultado")
 
+function limpar() {
+    resultado.innerHTML = ""
 }
+
 function inserir(n){
-    var resultado = document.querySelector("#resultado")
     resultado.innerHTML += n;
 }
+
 function voltar() {
-    var resultado = document.querySelector("#resultado")
     resultado.innerHTML = resultado.innerHTML.substring(0, resultado.innerHTML.length -1)
 }
+
 function calcular() {
-    var resultado = document.querySelector("#resultado")
     resultado.innerHTML = eval(resultado.innerHTML);
 }
